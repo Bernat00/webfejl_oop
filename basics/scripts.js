91fe34aea504a7be567c054929af87683a813a8f
Bernat00 <szabobernat06@gmail.com>
2024-12-02 15:50:34 +0100

    hazi

--- basics/scripts.js
+++ basics/scripts.js
@@ -43,11 +43,6 @@ class ClasosPlayer {
 }
 }
 
-const gomSzab = new ClasosPlayer("GomSzab");
-console.log(gomSzab);
-
-
-
 function Person(name){
     this.name = name;
 }
@@ -62,3 +57,70 @@ function Student(name, school){
 }
 
 Object.setPrototypeOf(Student.prototype, Person.prototype);
+
+
+class CPerson {
+    constructor(name) {
+        this.name = name;
+    }
+
+    getName(){
+        return this.name
+    }
+}
+
+class CStudent extends CPerson{
+    constructor(name, school){
+        super(name)
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+        
+}
+
+
+class Animal{
+    /**
+     * 
+     * @param {string} name 
+     */
+    constructor(name='noName'){
+        this.name = name;
+        this.sound = undefined;
+    }
+       
+    makeSound(){
+        if(this.sound === undefined)
+            console.log("sound is undifined");
+
+        console.log('the ' + this.name + ' says ' + this.sound);
+    }
+    
+}
+
+class Bird extends Animal{
+    constructor(name='noName'){
+        super(name);
+        this.sound = 'idk csirpel';
+    }
+
+    fly(){
+            console.log('the ' + this.name + 'flys');
+    }
+}
+
+class Mammal extends Animal{
+
+    constructor(name='noName'){
+        super(name);
+        this.sound = undefined;
+    }
+
+    walk(){
+        console.log('the ' + this.name + ' walks');
+    }
+    
+}
